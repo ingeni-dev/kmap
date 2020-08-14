1323a4805c70a4df98c1653bdae2f6445a7be863
porntip-ingeni <porntip.ingeni@gmail.com>
2020-08-14 16:29:11 +0700

    ล่าสุด 14/08/20
    ก่อนเพิ่มicon user

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,14 +2,23 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+// export const environment = {
+//   production: false,
+//   apiUrl: 'http://localhost:5000/',
+//   //apiUrl: 'http://localhost:5000/api/test',
+//   // apiUrl: 'http://localhost:5000/api/',
+//   whitelist: ['localhost:5000', '192.168.55.76', 'localhost:5001'],
+//   blacklisted: ['localhost:5000/users/login']
+// };
+
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:5000/',
-  //apiUrl: 'http://localhost:5000/api/test',
+  apiUrl: 'http://192.168.55.53:1189/api/',
   // apiUrl: 'http://localhost:5000/api/',
-  whitelist: ['localhost:5000', '192.168.55.76', 'localhost:5001'],
-  blacklisted: ['localhost:5000/users/login']
+  whitelist: ['192.168.55.53:1189', '192.168.55.76'],
+  blacklisted: ['192.168.55.53:1189/api','192.168.55.76/ToppWebApi/api/auth']
 };
+
 
 /*
  * For easier debugging in development mode, you can import the following file
