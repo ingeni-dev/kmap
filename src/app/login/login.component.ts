13b0f2c362e7eba3e0b2bb9c37f850f5be4f9741
porntip-ingeni <porntip.ingeni@gmail.com>
2020-08-18 16:31:50 +0700

    event enter

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -14,6 +14,7 @@ import { ActivationEnd, ActivatedRoute, Router } from '@angular/router';
   styleUrls: ['./login.component.css']
 })
 export class loginComponent {
+  value='';
   title = '';
   userPosition = '';
   model:loginForm={};
@@ -43,48 +44,58 @@ responseAdmin = {
         "del_stat": "N"
     }
 };
+
 constructor(private router: Router,
   private authService: AuthService,
   private toastrService: ToastrService){}
   
-onClick(){
+  onClick(){
 
-  console.log('1');
-  if(this.model.userId==='admin'){
-    this.userDetail = this.responseAdmin.user;
-    // this.positionDetail=this.responseAdmin.user;
-  } else {
-    this.userDetail = this.response.user;
-    // this.positionDetail=this.response.user;
+    console.log('1');
+    if(this.model.userId==='admin'){
+      this.userDetail = this.responseAdmin.user;
+      // this.positionDetail=this.responseAdmin.user;
+    } else {
+      this.userDetail = this.response.user;
+      // this.positionDetail=this.response.user;
+    }
+    this.positionDetail=this.response.user;
+    localStorage.setItem('userDetail',JSON.stringify(this.userDetail));
+    // localStorage.setItem('positionDetail',JSON.stringify(this.positionDetail));
+    this.router.navigate(['/layout']);
+    // return this.http.post(this.baseUrl + 'auth/login', this.model).pipe(
+    //   map((response: any) => {
+    //     // console.log(response);
+    //     response.body = {
+    //       "token": "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJuYW1laWQiOiI2MDAwNDIiLCJ1bmlxdWVfbmFtZSI6WyLguKXguLLguKDguKfguLHguJUg4LiV4Lix4LmJ4LiH4Lib4Li04Lii4Liw4LiB4Li44LilIiwiQjAwIiwiT1BQIl0sIm5iZiI6MTU5NDI4MDMzMywiZXhwIjoxNTk0MzY2NzMzLCJpYXQiOjE1OTQyODAzMzN9.1YCabqUZPMjofh0QtQC3ndvjB_bZ2-WxzwxHSiNxLsk-PZT_0uxC7Scu_1_fJxTIqPUqcvXXREkiir9DqWBLeA",
+    //       "user": {
+    //           "user_id": "600042",
+    //           "user_name": "ลาภวัต ตั้งปิยะกุล",
+    //           "unit_id": "B00",
+    //           "unit_desc": "สารสนเทศ IT",
+    //           "del_stat": "N"
+    //       }
+    //   }
+    //   console.logRes
+    //     // const user = response;
+    //     // if (user) {
+    //     //   console.log(user);
+    //     //   localStorage.setItem('id',user.user.user_id);
+    //     // }
+    //   })
+    // );
+
+
   }
-  this.positionDetail=this.response.user;
-  localStorage.setItem('userDetail',JSON.stringify(this.userDetail));
-  // localStorage.setItem('positionDetail',JSON.stringify(this.positionDetail));
-  this.router.navigate(['/layout']);
-  // return this.http.post(this.baseUrl + 'auth/login', this.model).pipe(
-  //   map((response: any) => {
-  //     // console.log(response);
-  //     response.body = {
-  //       "token": "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJuYW1laWQiOiI2MDAwNDIiLCJ1bmlxdWVfbmFtZSI6WyLguKXguLLguKDguKfguLHguJUg4LiV4Lix4LmJ4LiH4Lib4Li04Lii4Liw4LiB4Li44LilIiwiQjAwIiwiT1BQIl0sIm5iZiI6MTU5NDI4MDMzMywiZXhwIjoxNTk0MzY2NzMzLCJpYXQiOjE1OTQyODAzMzN9.1YCabqUZPMjofh0QtQC3ndvjB_bZ2-WxzwxHSiNxLsk-PZT_0uxC7Scu_1_fJxTIqPUqcvXXREkiir9DqWBLeA",
-  //       "user": {
-  //           "user_id": "600042",
-  //           "user_name": "ลาภวัต ตั้งปิยะกุล",
-  //           "unit_id": "B00",
-  //           "unit_desc": "สารสนเทศ IT",
-  //           "del_stat": "N"
-  //       }
-  //   }
-  //   console.logRes
-  //     // const user = response;
-  //     // if (user) {
-  //     //   console.log(user);
-  //     //   localStorage.setItem('id',user.user.user_id);
-  //     // }
-  //   })
-  // );
 
+  keyEnter(event:KeyboardEvent) {
+   
+    if( event.keyCode == 13 || event.which == 13) {
+      this.login()
+    }
+    
+  }
 
-}
   loading: Boolean = false;
  
   login() {
@@ -102,6 +113,7 @@ onClick(){
     //   this.loading = false;
     // });
   }
+  
 }
  interface loginForm{
   userId?:string;
